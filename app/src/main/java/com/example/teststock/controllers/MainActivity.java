db687123814669a4e755602be3b17eb0fabecc96
Gillou <gilles.hubert97@gmail.com>
2020-12-06 17:50:02 +0100

    Séparation Styles - Layout

--- app/src/main/java/com/example/teststock/controllers/MainActivity.java
+++ app/src/main/java/com/example/teststock/controllers/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.teststock.controllers;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -22,11 +19,8 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity{
 
     private RecyclerView recyclerView;
-    private final RecyclerView.LayoutManager recyclerViewLayoutManager = new LinearLayoutManager(this);
     private final List<Item> listItem = new ArrayList<>();
     private ItemAdapter itemAdapter;
-    private Drawable drawableDivider;
-    private DividerItemDecoration dividerItemDecoration;
     private Toolbar toolbar;
 
     @Override
@@ -37,16 +31,6 @@ public class MainActivity extends AppCompatActivity{
 
         toolbar = findViewById(R.id.activityMain_toolbar);
         setSupportActionBar(toolbar);
-
-        // RecyclerView initialization.
-        recyclerView = findViewById(R.id.activityMain_recyclerView_listItem);
-        recyclerView.setLayoutManager(recyclerViewLayoutManager);
-
-        // Separation in recylcerView.
-        drawableDivider = ContextCompat.getDrawable(recyclerView.getContext(), R.drawable.divider);
-        dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL);
-        dividerItemDecoration.setDrawable(drawableDivider);
-        recyclerView.addItemDecoration(dividerItemDecoration);
 
         // Item list initialization.
         listItem.add(new BasicItem("Gel WC", 4, "bouteille(s)"));
@@ -59,8 +43,11 @@ public class MainActivity extends AppCompatActivity{
         listItem.add(new BasicItem("Sac poubelle (petit noir)", 5, "rouleau(x)"));
         listItem.add(new BasicItem("Sac poubelle (grand noir)", 3, "rouleau(x)"));
         listItem.add(new BasicItem("Sac poubelle (blanc)", 1, "rouleau(x)"));
-
         itemAdapter = new ItemAdapter(listItem);
+
+        // RecyclerView initialization.
+        recyclerView = findViewById(R.id.activityMain_recyclerView_listItem);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(itemAdapter);
     }
 }
